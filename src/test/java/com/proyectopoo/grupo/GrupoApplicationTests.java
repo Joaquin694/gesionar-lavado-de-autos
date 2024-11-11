c5911d86a68275a7c430bbe40e37df052e8279b9
Joaquin694 <sebastianvasquezvalverde@gmail.com>
2024-11-11 11:46:40 -0500

    Preparando para la publicacion

---
+++ src/test/java/com/proyectopoo/grupo/GrupoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectopoo.grupo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrupoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
